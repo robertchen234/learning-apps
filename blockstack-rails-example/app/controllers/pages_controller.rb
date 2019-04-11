19094e3be72ce9e91608d4e91c29e6ebce7fc254
rob <robertchen234@gmail.com>
2019-04-11 10:16:50 -0400

    blockstack rails auth

---
+++ blockstack-rails-example/app/controllers/pages_controller.rb
@@ -0,0 +1,11 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def blockstack_callback
+    puts 'in the callback'
+    blockstack_info = request.env['omniauth.auth']
+    session[:blockstack_user] = blockstack_info
+    redirect_to '/'
+  end
+end
--- blockstack-rails-example/config/routes.rb
+++ blockstack-rails-example/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  get '/auth/blockstack/callback' => 'pages#blockstack_callback'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
